30f9117453fc02cf856737f4f7374695eddf1afd
Keila Lopez <keilalofra@gmail.com>
2022-09-26 18:21:31 -0700

    commit tree 0ba8035527b6e0c4006358ee020a5ffe1472bbe3

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Your code here
+// LEFT
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 100}px`;
+    }
+  }
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+
+  // RIGHT
+  function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    const rightNumbers = 173 + 40;
+    const right = parseInt(rightNumbers, 10)
+
+    if (left < 360) {
+        dodger.style.left = `${left+10}px`;
+    }
+  }
+
+  document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+  })
